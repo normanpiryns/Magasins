8b6dde043c650ed81f58da182214ec8a1ee40b7c
corentingoo <corentingoo@gmail.com>
2021-01-23 09:06:19 +0100

    correction de ma bètize

--- src/main/java/be/ifosup/servlet/ServletMag.java
+++ src/main/java/be/ifosup/servlet/ServletMag.java
@@ -5,7 +5,7 @@ import javax.servlet.http.*;
 import javax.servlet.annotation.*;
 import java.io.IOException;
 
-@WebServlet(name = "ServletMag", urlPatterns = {"/mag"})
+@WebServlet(name = "ServletMag", urlPatterns = {"/"})
 public class ServletMag extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
